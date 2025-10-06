bd21e4d4ae43ab3fcb185e4e2e0313169f8ca738
Olivia Milton (1000137301) <MILTOL1@student.op.ac.nz>
2025-10-07 11:51:56 +1300

    Title idea

---
+++ Game_Title/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Game_Title
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine(@"                                                                                                                     ");
+            Console.WriteLine(@"                                                                    .*#                                              ");
+            Console.WriteLine(@"                                                              .+% +*##@.                                             ");
+            Console.WriteLine(@"                                                             ..--+++**@.                                             ");
+            Console.WriteLine(@"                                                                     .@                                              ");
+            Console.WriteLine(@"                                                                     .@                                              ");
+            Console.WriteLine(@"                                                                     :@                                              ");
+            Console.WriteLine(@"                                                                    :%%=                                             ");
+            Console.WriteLine(@"                                                                  .* *##%%.                                          ");
+            Console.WriteLine(@"                                                                .**=##-*-%==                                         ");
+            Console.WriteLine(@"                                                              .+#:**#.:*-:#*#=:                                      ");
+            Console.WriteLine(@"                                                           ..@#:.@+#+ -+*. #*:#=#..                                  ");
+            Console.WriteLine(@"                                                         .-#*  :#+#=  #+*: .#**.*%-+:.                               ");
+            Console.WriteLine(@"                                                      .- *#+  .+*+#:   +++%. .#+#+.-#+-=:                            ");
+            Console.WriteLine(@"                                                   .* *#+.   :#++#.    #+++-   =++#- :#**---                         ");
+            Console.WriteLine(@"                                               =%####..    *++*#     .*+++%    .*+*#. .-#*##.%:                      ");
+            Console.WriteLine(@"                                      .::=@#######:.....:%####=......@#####=.....*###%:...=###%=:+-:.                ");
+            Console.WriteLine(@"                                      .#####%*+*==:..:+*+*+#%+==:...=##+++-:+###=+=%%*++...==+:..#%%%.               ");
+            Console.WriteLine(@"                                      .######:...-####=....*##***...*%#.....=#####%....-#####-...###%.               ");
+            Console.WriteLine(@"                                      .#*+++#:...-####=....*#*##*..:@%@*....=#####%:...-#####:...#+*%                ");
+            Console.WriteLine(@"                                      .#*+++#.   :#++#=....**###*..%%%@%#...=#**++%.   :#+++#:   #+*#                ");
+            Console.WriteLine(@"                                      .#*+++#.   :#++#-    +*++**.*##@@%#+. =*++++%.   :#+++#:   #+**                ");
+            Console.WriteLine(@"                                      .#*+++#:   :#++#-    +*++*#%##%@@@##*.=+++++#.   :#+++#.   #+*+                ");
+            Console.WriteLine(@"                                      .#*+++#:   .#++*-    ++++#####@@@@%####+++++#.   :#+++#.   #+#+                ");
+            Console.WriteLine(@"                                      .#*+++#:   .#++*-    +++%###%@@@@@@@###%*+++#:   :#+++#.   #+#=                ");
+            Console.WriteLine(@"                                      .#*+++#-   .#++*-    +#####@@@@@@@@@%#####*+#:   :#+++%.   #*#-                ");
+            Console.WriteLine(@"                                       ######-.  .#++*- :+####%%@@@@@@@@@@@@%###**%*+: :#***%....###:                ");
+            Console.WriteLine(@"                                       ######=...:####=.*%%%%%@@@@@@@@@@@@@@%%%%%%%=...-####%:...###:                ");
+            Console.WriteLine(@"                                                                                                                     ");
+            Console.WriteLine(@"                                 ___  _                                         _        ______          _            ");
+            Console.WriteLine(@"                                / _ \| |                                       | |       | ___ \        | |           ");
+            Console.WriteLine(@"                               / /_\ \ |__  _   _ ___  ___ _ __ ___   ___ _ __ | |_      | |_/ /_ _ _ __| | __        ");
+            Console.WriteLine(@"                               |  _  | '_ \| | | / __|/ _ \ '_ ` _ \ / _ \ '_ \| __|     |  __/ _` | '__| |/ /        ");
+            Console.WriteLine(@"                               | | | | |_) | |_| \__ \  __/ | | | | |  __/ | | | |_      | | | (_| | |  |   <         ");
+            Console.WriteLine(@"                               \_| |_/_.__/ \__,_|___/\___|_| |_| |_|\___|_| |_|\__|     \_|  \__,_|_|  |_|\_\        ");
+            Console.WriteLine();
+            Console.WriteLine("                                                  There's something strange about this place...                       ");
+            Console.WriteLine();
+            Console.WriteLine("                                                          Press ENTER to proceed                                      ");
+            Console.ReadLine();
+           
+
+
+
+
+
+
+
+
+
+
+
+                                                                           
+                                                                           
+
+ 
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+        }
+    }
+}
