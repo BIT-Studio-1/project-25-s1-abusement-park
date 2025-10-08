c15fe41ac0a31bb4f3f5cd464e53299c9819c02d
Thomas Roberts (1000113228) <ROBETJ4@student.op.ac.nz>
2025-10-08 13:34:48 +1300

    Added computer betting

--- Liars Dice/Program.cs
+++ Liars Dice/Program.cs
@@ -24,6 +24,7 @@
                 Press Enter to begin...
                 """);
 
+            // User input
             Console.Write("> ");
             Console.ReadLine();
 
@@ -44,6 +45,7 @@
                 computerDice[i] = random.Next(1, 7);
             }
 
+            // Prompt to "roll"
             Console.Clear();
             Console.WriteLine("Press ENTER to roll...");
             Console.Write("> ");
@@ -57,6 +59,7 @@
             {
                 if (playerTurn)
                 {
+                    // Player betting //
                     int[] newBet = new int[2];
                     while ((newBet[0] == 0 || newBet[1] == 0) && bullshit == false)
                     {
@@ -73,12 +76,17 @@
                         Console.WriteLine("What is your bet? [Number of dice] [Value of dice] or call 'bullshit'");
                         Console.Write("> ");
                         string playerInput = Console.ReadLine();
+
+                        // Strips the user input
                         playerInput = playerInput.Replace(" ", string.Empty).Replace(",", string.Empty);
 
                         // Check if input is valid
                         if (int.TryParse(playerInput, out int result) && (playerInput.Length == 2))
                         {
+                            // Splits the user input into its own variables
                             int numberOf = Int32.Parse(playerInput[0].ToString()), valueOf = Int32.Parse(playerInput[1].ToString());
+
+                            // Checks if the user input is within the betting boundaries
                             if (numberOf > currentBet[0] && numberOf <= 6)
                             {
                                 if (valueOf > 0 && valueOf <= 6)
@@ -116,10 +124,12 @@
                         }
                         else
                         {
+                            // Checks if the player called bullshit
                             if (playerInput.ToLower() == "bullshit")
                             {
                                 bullshit = true;
                             }
+                            // User feed back for not a valid option
                             else
                             {
                                 Console.WriteLine($"{playerInput[0]}, {playerInput[1]} is not a valid input. A valid input looks like:\n3 6 or 36 (Three 6's");
@@ -129,7 +139,7 @@
                         }
                     }
 
-                    // Set the bet
+                    // Sets the bet
                     if (bullshit == false)
                     {
                         currentBet[0] = newBet[0];
@@ -139,9 +149,68 @@
                 }
                 else
                 {
-                    playerTurn = true;
+                    // Computer Betting //
+                    int[] newBet = new int[2];
+                    Console.Clear();
+                    Console.WriteLine("The current bet is: [Number of dice] [Value of dice]");
+                    Console.WriteLine($"{currentBet[0]}, {currentBet[1]}");
+                    Console.Write("\nThe computer is thinking");
+                    for (int i=0;i<3;i++)
+                    {
+                        Console.Write(".");
+                        Thread.Sleep(1000);
+                    }
+                    
+                    // Generates a random chance for the computer to call bullshit
+                    int bullshitChance = random.Next(10);
+                    switch (bullshitChance)
+                    {
+                        case 0:
+                            // Calls bullshit
+                            bullshit = true;
+                            Console.WriteLine("\n\nI call BULLSHIT!");
+                            Console.WriteLine("\nPress ENTER to continue...");
+                            Console.ReadLine();
+                            break;
+                        default:
+                            // Makes a bet of same count
+                            if (currentBet[1] < 6)
+                            {
+                                newBet[0] = currentBet[0];
+                                newBet[1] = random.Next((currentBet[1] + 1), 7);
+                            }
+                            else
+                            {
+                                // Makes a bet of higher count or forces bullshit
+                                if (currentBet[0] == 6)
+                                {
+                                    bullshit = true;
+                                    Console.WriteLine("I call BULLSHIT!");
+                                    Console.WriteLine("\nPress ENTER to continue...");
+                                    Console.ReadLine();
+                                }
+                                else
+                                {
+                                    newBet[0] = currentBet[0] + 1;
+                                    newBet[1] = random.Next(1, 7);
+                                }
+                            }
+                            break;
+                    }
+
+                    // Sets the bet
+                    if (bullshit == false)
+                    {
+                        currentBet[0] = newBet[0];
+                        currentBet[1] = newBet[1];
+                        playerTurn = true;
+                    }
                 }
             }
+
+            Console.Clear();
+            Console.WriteLine("Someone called bullshit");
+            Console.ReadLine();
         }
     }
 }
