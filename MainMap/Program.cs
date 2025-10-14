e6850a41ace07d743dad0d5a84ea925ad84116aa
beldonholt <beldon.holt@gmail.com>
2025-10-14 13:18:02 +1300

    reading map from file

--- MainMap/Program.cs
+++ MainMap/Program.cs
@@ -1,19 +1,48 @@
 ﻿using System.Drawing;
 using System.Net;
+using System.IO;
 
 namespace MainMap
 {
     internal class Program
     {
-        Player? user;
-        char[,]? map;
-        Dictionary<int[], String> locations;
+        static Player? user;
+        static char[][]? map;
+        static Dictionary<int[], String>? locations;
 
-        private void SetupMap(){
+        public Program() { }
 
+        static void Main(string[] args)
+        {
+            SetupMap();
         }
 
-        public void Nav(){
+
+        public static void SetupMap(){
+            try
+            {
+                string filePath = "Map.txt";
+                List<char[]> lines = new List<char[]>();
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lines.Add(line.ToCharArray());
+                    }
+                }
+                map = lines.ToArray();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public static void Nav(){
             user = new Player(0, 0);
             do
             {
@@ -31,7 +60,7 @@ namespace MainMap
                 // Only move if next spot is a path (0)
                 if ((newY > 0 && newY <= map.Length) && (newX > 0 && newX <= map.GetLength(0)))
                 {
-                    if (map[newY, newX] == 0)
+                    if (map[newY][newX] == 0)
                     {
                         // Erase old position
                         Console.SetCursorPosition((user.x * 2), user.y);
