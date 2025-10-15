08d0f7f1c0e84b856acebc44bb61a63a4676d0cf
Olivia Milton (1000137301) <MILTOL1@student.op.ac.nz>
2025-10-15 13:44:57 +1300

    added user input and checks invalid

--- BS_AnimatedGrids/Program.cs
+++ BS_AnimatedGrids/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace BS_AnimatedGrids
 {
@@ -10,42 +11,62 @@ namespace BS_AnimatedGrids
             char [,] BSplayerGrid = new char [BSgridSize, BSgridSize];
             char[,] BSkrakenGrid = new char [BSgridSize, BSgridSize];
 
-            //launch grids
-            for (int y = 0; y < BSgridSize; y++)
+            //Computer Ship         
+            BSkrakenGrid[5, 3] = '#';
+            BSkrakenGrid[5, 4] = '#';
+            BSkrakenGrid[5, 5] = '#';
+            BSkrakenGrid[5, 6] = '#';
+
+            int KrakenHits = 0;
+
+            //launch game
+            while (KrakenHits <= 4)
             {
-                for (int x = 0; x < BSgridSize; x++)
-                {
-                    BSplayerGrid[y, x] = '*';
-                    BSkrakenGrid[y, x] = '*';
-                }
-            }
-            
-            for (int y = 0; y < BSgridSize; y++)
-            {
-                Console.Write("  ");
-                Console.Write(y);
-            }
-            Console.WriteLine();
-            for (int x = 0; x < BSgridSize; x++)
-            {
-                Console.Write(x + " ");
                 for (int y = 0; y < BSgridSize; y++)
                 {
-                    Console.Write(BSplayerGrid[x, y]+ "  ");
+                    for (int x = 0; x < BSgridSize; x++)
+                    {
+                        BSplayerGrid[y, x] = '*';
+                        BSkrakenGrid[y, x] = '*';
+                    }
+                }
+
+                for (int y = 0; y < BSgridSize; y++)
+                {
+                    Console.Write("  ");
+                    Console.Write(y);
                 }
                 Console.WriteLine();
+                for (int x = 0; x < BSgridSize; x++)
+                {
+                    Console.Write(x + " ");
+                    for (int y = 0; y < BSgridSize; y++)
+                    {
+                        Console.Write(BSplayerGrid[x, y] + "  ");
+                    }
+                    Console.WriteLine();
+                }
+
+                //userInput
+                Console.WriteLine("Enter coordinates to strike:");
+                Console.Write("X: ");
+                string tempX = Console.ReadLine();
+                Console.Write("Y: ");
+                string tempY = Console.ReadLine();
+                int BSuserInputX = Convert.ToInt32(tempX);
+                int BSuserInputY = Convert.ToInt32(tempY);
+
+                if (BSuserInputX < 0 || BSuserInputX >= BSgridSize || BSuserInputY < 0 || BSuserInputY >= BSgridSize)
+                {
+                    Console.WriteLine("Invalid Coordinates. Please try again.");
+                    continue;
+                }
+
+
+
+                Console.ReadLine();
+
             }
-
-            //userInput
-            Console.WriteLine("Enter coordinates to strike:");
-            Console.Write("X: ");
-            string tempX = Console.ReadLine();
-            int BSuserInputX = Convert.ToInt32(tempX);
-            Console.Write("Y: ");
-            string tempY =  Console.ReadLine();
-            int BSuserInputY = Convert.ToInt32(tempY);
-
-            Console.ReadLine();
         }
     }
 }
