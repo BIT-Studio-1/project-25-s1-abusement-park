7fe1f48732741b702af7115321b724094b5371ae
Olivia Milton (1000137301) <MILTOL1@student.op.ac.nz>
2025-10-15 13:19:44 +1300

    created BS grids, using arrays

---
+++ BS_AnimatedGrids/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BS_AnimatedGrids
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int BSgridSize = 7;
+            char [,] BSplayerGrid = new char [BSgridSize, BSgridSize];
+            char[,] BSkrakenGrid = new char [BSgridSize, BSgridSize];
+
+            //launch grids
+            for (int y = 0; y < BSgridSize; y++)
+            {
+                for (int x = 0; x < BSgridSize; x++)
+                {
+                    BSplayerGrid[y, x] = '*';
+                    BSkrakenGrid[y, x] = '*';
+                }
+            }
+            
+            for (int y = 0; y < BSgridSize; y++)
+            {
+                Console.Write("  ");
+                Console.Write(y);
+            }
+            Console.WriteLine();
+            for (int x = 0; x < BSgridSize; x++)
+            {
+                Console.Write(x + " ");
+                for (int y = 0; y < BSgridSize; y++)
+                {
+                    Console.Write(BSplayerGrid[x, y]+ "  ");
+                }
+                Console.WriteLine();
+            }
+
+            //userInput
+            Console.WriteLine("Enter coordinates to strike:");
+            Console.Write("X: ");
+            string tempX = Console.ReadLine();
+            int BSuserInputX = Convert.ToInt32(tempX);
+            Console.Write("Y: ");
+            string tempY =  Console.ReadLine();
+            int BSuserInputY = Convert.ToInt32(tempY);
+
+            Console.ReadLine();
+        }
+    }
+}
