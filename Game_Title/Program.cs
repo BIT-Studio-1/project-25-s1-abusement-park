fd3e1dd155121928c81e04db24c42762f80c9f6d
Thomas Roberts (1000113228) <ROBETJ4@student.op.ac.nz>
2025-10-21 11:35:34 +1300

    moved clown from maze

--- Game_Title/Program.cs
+++ Game_Title/Program.cs
@@ -24,6 +24,32 @@ namespace Game_Title
 
         private const int SW_MAXIMIZE = 3;
 
+        static void CreepyClown()
+        {
+            Random rand = new Random();
+            Console.Clear();
+            Console.WriteLine("                      ..:::::::::::::..\r\n                .:::::''              ``:::.\r\n              .:;'                        `::.\r\n           ..::'                            `::.\r\n          ::'                                  ::.:'\r\n      `::.::                                    ::.\r\n    .::::::::'                                `:.:::.    .:':'\r\n:::::::::::::.          .:.                .:. ` :::::::::':::\r\n:::.::::::::::::'       :::                :::    :::::::::':::'\r\n..::::::::::::'          ' `                ' `   .::::::' :::'\r\n::::::::::::'  `:.   .:::::::.          .:::::::.:: .:' :'.::'\r\n::::::::::::    `::.::'     `::.      .::'     `::.::':'.:::'\r\n::::::::::::      .::'        `:;  . .::'        `:;:'.::''\r\n:::::::::::'.     ::'    .    .:: :  ::'    .    .:::::''\r\n:`::::::::::::.:  `::.  :O: .::;' :  `::.  :O: .::;'::'\r\n   `::::::`::`:.    `:::::::::'   :.   `:::::::::':'''\r\n       `````:`::.     , .         `:.        , . `::.\r\n            :: `::.   :::      ..::::::::..  :::  `::\r\n      .::::'::. `::.  `:'     :::::::::::::; `:'   :;\r\n            ::'    ::.   .::'  ``:::::::;'' :.   .:'\r\n            `::    `::  ::'        ::       .::  :'\r\n             ::.    :'.::::::.    :  :   .::::. .:::.\r\n:.           `::.     :::'  ``::::. .::::'' `::::' `::.\r\n`::.          `::.    `:::. ::.  `::::' .:: ::::;    `::\r\n:.`:.          `::.     `::. `:::.    .::'  ::;'     .:;.\r\n ::`::.          `::.     `::.  `::. .::' .:;':'     :;':.\r\n::':``:::::.       `::.     `::. `::::'  .:;':'     .;':':\r\n: .:`:::':`:::::.   `::.      `:::.   .::;'.:'  .::;'' ';:\r\n..::': :. ::::. `::::::`::..      `:::::'  .:':::'::.:: :':\r\n:' :'.:::. `:: :: ::. .::`::.   .     . .:;':' ::'`:: :::'\r\n: ::.:. `:  `::'  `:: ::'::`::::::::::::;' :: .:' .::: ;:'\r\n::.::.:::: .:: :.  `:':'  ::.:'`::. .::':.::' :: .::''::'\r\n`:::`::.`:.::' ::  .: ::  `::'  `:: :' .::' ::.:.::' :;\r\n   `::::::.`:. .:. :: `::.:: ::  `::. .:: ::.`:::':.:;'\r\n         `::::::::::...:::'  `::.:'`:.::'.:.:;' .:;'\r\n                    `::::::::::::::::::::'.::;:;'");
+            int i = rand.Next(1, 10);
+            switch (i)
+            {
+                case 1: Console.WriteLine("I can see you"); break;
+                case 2: Console.WriteLine("Are you ready to smile... FOREVER?!"); break;
+                case 3: Console.WriteLine("Don't run, we just want to play."); break;
+                case 4: Console.WriteLine("88888888ba                           \r\n88      \"8b                          \r\n88      ,8P                          \r\n88aaaaaa8P'  ,adPPYba,   ,adPPYba,   \r\n88\"\"\"\"\"\"8b, a8\"     \"8a a8\"     \"8a  \r\n88      `8b 8b       d8 8b       d8  \r\n88      a8P \"8a,   ,a8\" \"8a,   ,a8\"  \r\n88888888P\"   `\"YbbdP\"'   `\"YbbdP\"' "); break;
+                case 5: Console.WriteLine("I can smell you"); break;
+                case 6: Console.WriteLine("Hey kid, wanna buy some balloons"); break;
+                case 7: Console.WriteLine("We all float down here"); break;
+                case 8: Console.WriteLine("Nice eyes you have there, they look perfect to juggle"); break;
+                case 9: Console.WriteLine("Don’t be shy… the fun’s just beginning!"); break;
+                case 10: Console.WriteLine("Oops! I dropped my nose… guess I’ll have to borrow yours!"); break;
+                case 11: Console.WriteLine("The circus left town… but I stayed behind."); break;
+                case 12: Console.WriteLine("I used to make people laugh… now I make them disappear."); break;
+                default: Console.WriteLine("Hehehehe"); break;
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White;
@@ -150,20 +176,26 @@ namespace Game_Title
                 {
                     case 1:
                         LiarsDiceGame.LiarsDiceMain();
+                        CreepyClown();
                         break;
                     case 2:
                         BattleShip.BattleShipMain();
+                        CreepyClown();
                         break;
                     case 3:
                         Blackjack.BlackJackMain();
+                        CreepyClown();
                         break;
                     case 4:
                         CoinGame.CoinGameMain();
+                        CreepyClown();
                         break;
                     case 5:
                         Mazing.MazingMain();
+                        CreepyClown();
                         break;
                     case 0:
+                        CreepyClown();
                         break;
                     default:
                         Console.WriteLine("That wasn't an option...");
