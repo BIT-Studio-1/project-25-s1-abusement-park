f93fed2f2c21d7aeab3a527fa6461e13deffcc85
Thomas Roberts (1000113228) <ROBETJ4@student.op.ac.nz>
2026-03-03 11:41:23 +1300

    Removed the bullshit colours and beeps

--- Game_Title/Games/CoinGame.cs
+++ Game_Title/Games/CoinGame.cs
@@ -29,7 +29,6 @@ namespace Game_Title
 
             while (keepPlaying)
             {
-                gamesPlayed++;  // increment games played every round
                 string userGuess = " ";
 
                 while (userGuess[0] != 'h' && userGuess[0] != 't')
@@ -43,50 +42,40 @@ namespace Game_Title
                     }
                     else if (userGuess[0] != 'h' && userGuess[0] != 't')
                     {
-                        Console.WriteLine("\n!!! Invalid input. Please type 'Heads' or 'Tails'.\n");      //Add input validation to only accept 'Heads' or 'Tails
+                        Console.WriteLine("\n!!! Invalid input. Please type 'Heads' or 'Tails'.\n");
                     }
                 }
 
                 string coinFlip = rand.Next(0, 2) == 0 ? "heads" : "tails";
 
-                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("\nFlipping the coin in...");
-                Console.ResetColor();
-                Console.ForegroundColor = ConsoleColor.Blue;
                 for (int i = 3; i > 0; i--)
                 {
                     Console.Write(i + "... ");
-                    System.Threading.Thread.Sleep(500); // Pause for half a second
+                    Thread.Sleep(500); // Pause for half a second
                 }
                 Console.WriteLine("\n");
-                Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine(@"
       _______       
      /       \
     |  " + (coinFlip == "heads" ? "  H" : "  T") + @"    |                                                                                                     
      \_______/
 ");
-                Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine($"The coin landed on: {coinFlip.ToUpper()}!\n");
-                Console.ResetColor();
                 if (userGuess == coinFlip)
                 {
                     wins++; // Increment wins counter on correct guess
-                    Console.Beep(); //Add beep for correct guess
-                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Congrats! You guessed correctly. You win!\n");                  //Highlights "Congrats! You guessed correctly. You win!" in green for positive feedback
-                    Console.ResetColor();
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;                                         //Add red color to incorrect guess message
                     Console.WriteLine("Sorry, you guessed wrong. Try again!\n");
-                    Console.ResetColor();
                 }
+
+                gamesPlayed++;  // increment games played every round
+
                 // (Added input validation for "play again" prompt)
-                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.Write("Do you want to play again? (yes/no): ");
-                Console.ResetColor();
                 string playAgain = Console.ReadLine().Trim().ToLower();
 
                 // Keep asking until valid input is entered
@@ -102,11 +91,8 @@ namespace Game_Title
 
                     if (playAgain == "no")
                     {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine("\nThanks for playing! Goodbye!");
-                        Console.ResetColor();
 
-                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine(@"
                        _____          __  __ ______    ______      ________ _____  
                       / ____|   /\   |  \/  |  ____|  / __ \ \    / /  ____|  __ \ 
@@ -115,7 +101,6 @@ namespace Game_Title
                      | |__| |/ ____ \| |  | | |____  | |__| | \  /  | |____| | \ \ 
                       \_____/_/    \_\_|  |_|______|  \____/   \/   |______|_|  \_\
                      ");
-                        Console.ResetColor();
 
                         Console.WriteLine($"\nGames played: {gamesPlayed}, Wins: {wins}");
                         Console.ReadLine(); // Pause before closing
